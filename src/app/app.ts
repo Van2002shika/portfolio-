801edb9514a41a97b2c5558aa87479c6588004cf
Van2002shika <vanshikaparihaar0@gmail.com>
2025-11-13 22:40:23 +0530

    first commit

--- src/app/app.ts
+++ src/app/app.ts
@@ -5,8 +5,36 @@ import { RouterOutlet } from '@angular/router';
   selector: 'app-root',
   imports: [RouterOutlet],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
 export class App {
-  protected readonly title = signal('portfolio-');
+  fullText = 'Amanat Singh Bhadouriya';
+  displayedText = '';
+  private index = 0;
+
+  ngOnInit() {
+    this.typeWriter();
+  }
+
+  activateTab(id: string) {
+    const triggerEl = document.querySelector(`[data-bs-target="#${id}"]`);
+    if (triggerEl) {
+      // @ts-ignore
+      const tab = new bootstrap.Tab(triggerEl);
+      tab.show();
+    }
+  }
+
+  typeWriter() {
+    if (this.index < this.fullText.length) {
+      this.displayedText += this.fullText.charAt(this.index);
+      this.index++;
+      setTimeout(() => this.typeWriter(), 100);
+    }
+  }
+  expandedCards: boolean[] = [false, false, false];
+
+  toggleCard(index: number): void {
+    this.expandedCards[index] = !this.expandedCards[index];
+  }
 }
