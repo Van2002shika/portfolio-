7db4dd3745be3edecc90a7c50412ec7a985cdbf9
Van2002shika <vanshikaparihaar0@gmail.com>
2025-11-21 22:36:45 +0530

    new

--- src/app/app.ts
+++ src/app/app.ts
@@ -44,4 +44,47 @@ export class App {
   toggleCard(i: number) {
     this.expandedCards[i] = !this.expandedCards[i];
   }
+
+  expandedProjects: boolean[] = [];
+  projects = [
+    {
+      title: 'Twitter Clone',
+      img: 'twitter_newbird_boxed_whiteonblue.webp',
+      desc: [
+        'A Twitter-style social platform built with React.js.',
+        'Real-time posts, likes, and comments.',
+        'Profile system with user feeds.',
+      ],
+      github: 'https://github.com/Amanat038',
+    },
+
+    {
+      title: 'Task Manager App',
+      img: 'Screenshot 2025-11-21 213800.png',
+      desc: [
+        'Built using React.js and react-beautiful-dnd.',
+        'Drag-and-drop task management.',
+        'Fully responsive design with Tailwind CSS.',
+      ],
+      live: 'https://drag-and-drop789.netlify.app/',
+      github: 'https://github.com/Amanat038/Task-Manage-system',
+    },
+
+    {
+      title: 'Real-Time Chat Application',
+      img: 'Screenshot 2025-11-21 213836.png',
+      desc: [
+        'Built using React.js + Socket.IO.',
+        'Real-time chatting with multi-user rooms.',
+        'Express.js backend and responsive UI.',
+      ],
+      live: 'https://chat-application-08060601.netlify.app/',
+      github: 'https://github.com/Amanat038',
+    },
+  ];
+
+  // Toggle expand
+  toggleProject(i: number) {
+    this.expandedProjects[i] = !this.expandedProjects[i];
+  }
 }
